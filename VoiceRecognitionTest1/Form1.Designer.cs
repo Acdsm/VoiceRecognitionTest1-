62246f2160914d3073caa0ace402089cdabe10ac
Acdsm <38685173+Acdsm@users.noreply.github.com>
2018-09-08 23:37:01 +0100

    Initial commit

---
+++ VoiceRecognitionTest1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace VoiceRecognitionTest1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnEnabled = new System.Windows.Forms.Button();
+            this.BtnDisable = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // BtnEnabled
+            // 
+            this.BtnEnabled.Location = new System.Drawing.Point(16, 234);
+            this.BtnEnabled.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnEnabled.Name = "BtnEnabled";
+            this.BtnEnabled.Size = new System.Drawing.Size(157, 28);
+            this.BtnEnabled.TabIndex = 0;
+            this.BtnEnabled.Text = " Enable Voice Control";
+            this.BtnEnabled.UseVisualStyleBackColor = true;
+            this.BtnEnabled.Click += new System.EventHandler(this.BtnEnabled_Click);
+            // 
+            // BtnDisable
+            // 
+            this.BtnDisable.Enabled = false;
+            this.BtnDisable.Location = new System.Drawing.Point(205, 234);
+            this.BtnDisable.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnDisable.Name = "BtnDisable";
+            this.BtnDisable.Size = new System.Drawing.Size(171, 28);
+            this.BtnDisable.TabIndex = 1;
+            this.BtnDisable.Text = "Disable Voice Control";
+            this.BtnDisable.UseVisualStyleBackColor = true;
+            this.BtnDisable.Click += new System.EventHandler(this.BtnDisable_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(3, 2);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(385, 223);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "-Log-";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 266);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnDisable);
+            this.Controls.Add(this.BtnEnabled);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Speech Recognition Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnEnabled;
+        private System.Windows.Forms.Button BtnDisable;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ VoiceRecognitionTest1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition; 
+
+namespace VoiceRecognitionTest1
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // recognizes multiple audio inputs
+        private void BtnEnabled_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            BtnDisable.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "hello", "print my name", });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
+        }
+
+        private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                case "hello":
+                    MessageBox.Show("hello Andy, how are you");
+                    break;
+                case "print my name":
+                    richTextBox1.Text += "\nAndy";
+                    break;
+
+            }
+        }
+
+        private void BtnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            BtnDisable.Enabled = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
